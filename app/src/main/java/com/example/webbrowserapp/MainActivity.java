e1466f3e74ec3cf4785140407f69025861db4e24
Anzor0 <k.plodovskiy@gmail.com>
2022-05-29 22:07:05 +0300

    final

--- app/src/main/java/com/example/webbrowserapp/MainActivity.java
+++ app/src/main/java/com/example/webbrowserapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.webbrowserapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -9,6 +11,7 @@ import android.util.Patterns;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
@@ -25,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     ImageView clearUrl;
     WebView webView;
     ProgressBar progressBar;
+    ImageView webBack,webForward,webRefresh,webShare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +39,10 @@ public class MainActivity extends AppCompatActivity {
         clearUrl = findViewById(R.id.clear_icon);
         progressBar = findViewById(R.id.progress_bar);
         webView = findViewById(R.id.web_view);
+        webBack = findViewById(R.id.web_back);
+        webForward = findViewById(R.id.web_forward);
+        webRefresh = findViewById(R.id.web_refresh);
+        webShare = findViewById(R.id.web_share);
 
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -50,15 +58,62 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        loadMyUrl("google.com");
+        loadMyUrl("https://www.google.com/");
+
         urlInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
                 if (i == EditorInfo.IME_ACTION_GO || i == EditorInfo.IME_ACTION_DONE){
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(urlInput.getWindowToken(), 0);
                     loadMyUrl(urlInput.getText().toString());
                     return true;
                 }
                 return false;
+            }
+        });
+
+
+        clearUrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                urlInput.setText("");
+            }
+        });
+
+        webBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(webView.canGoBack()){
+                    webView.goBack();
+                }
+            }
+        });
+
+        webForward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (webView.canGoForward()) {
+                    webView.goForward();
+                }
+            }
+        });
+
+        webRefresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webView.reload();
+            }
+        });
+
+        webShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, webView.getUrl());
+                intent.setType("text/plain");
+                startActivity(intent);
             }
         });
 
@@ -73,6 +128,16 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+
     class MyWebViewClient extends WebViewClient{
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
@@ -82,6 +147,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
             super.onPageStarted(view, url, favicon);
+            urlInput.setText(webView.getUrl());
             progressBar.setVisibility(View.VISIBLE);
         }
 
